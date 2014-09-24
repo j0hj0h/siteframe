e3e0c595152eb136fbbbfdf591f5abc2e9044e90
joh <git@sights.de>
2014-09-24 12:19:50 +0200

    ...

---
+++ config.local.inc.php
@@ -0,0 +1,7 @@
+<?php defined('EXECUTE') or die("Access Denied.");
+
+// system:
+define('ABSPATH', dirname(__FILE__) . '/');
+define('BASE_URL', "http://localhost:8888/site-frame/");
+date_default_timezone_set("Europe/Berlin");
+define('DEBUG_MODE', true);
