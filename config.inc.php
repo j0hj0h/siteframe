d669e5a0f0ca4d52407fd4e3345c5ae3f709ea49
joh <git@sights.de>
2014-09-24 12:18:52 +0200

    comments translated to english mail errors removed config.local

--- config.inc.php
+++ config.inc.php
@@ -1,12 +1,1 @@
-<?php defined('EXECUTE') or die("Access Denied."); 
-
-// System:
-
-define('ABSPATH', dirname(__FILE__) . '/');
-
-define('BASE_URL', "http://localhost:8888/site-frame/");
-
-date_default_timezone_set("Europe/Berlin");
-
-define('DEBUG_MODE', true);
-define('MAIL_ERRORS', false);
+<?php defined('EXECUTE') or die("Access Denied.");
--- index.php
+++ index.php
@@ -1,29 +1,16 @@
 <?php
 
-// flag das sicherstellt, dass keine includes direkt aufgerufen werden
+// flag to ensure, that includes aren't called directly
 define('EXECUTE', true);
 
-// errors als exceptions behandeln
-function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-	if($errno != E_NOTICE) { // E_NOTICE is ok
-		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-	}
-}
-set_error_handler("exception_error_handler");
-
-// umschließendes try/catch
-try {
-
-// config laden
+// load config laden
 require("config.inc.php");
 
 // debug mode initialisieren
 if(DEBUG_MODE) {
+	ini_set('display_errors', 1);
 	error_reporting(E_ALL);
 }
-
-// session starten für fontorder, muss vor der ersten ausgabe passieren
-session_start();
 
 // 404 function
 function response404() {
@@ -33,19 +20,19 @@ function response404() {
     exit();	
 }
 
-// URL splitten wenn "?" enthalten
+// split URL if "?" contained
 if(strpos($_SERVER['REQUEST_URI'], "?")) {
 	list($path, $parameter) = explode('?', $_SERVER['REQUEST_URI']);
 } else {
 	$path = $_SERVER['REQUEST_URI'];
 }
 
-// URL prüfen, bei nicht alpha-numerischen Zeichen 404 senden
+// if URL contains unvailed characters send 404
 if(preg_match('/[^a-zA-Z0-9\/\-\_]/', $path)) {
 	response404();
 }
 
-// URL trimmen und Pfad zu dieser Datei (index.php) entfernen
+// trim URL and remove path to this file (index.php)
 $path = explode('/', rtrim($path,"/") );
 $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
 for($i= 0;$i < sizeof($scriptName);$i++) {
@@ -55,20 +42,20 @@ for($i= 0;$i < sizeof($scriptName);$i++) {
 }
 $path = array_values($path);
 
-// Homepage setzen wenn kein Pfad übergeben wurde
+// set home if no path
 if( empty($path[0]) ) {
 	$path[0] = "home";
 }
 
-// Systempfad zu der zu inkludierenden Datei setzen
+// set system path to the page include
 $page = ABSPATH . "pages/" . implode("/", $path) . ".inc.php";
 
-// Prüfen, ob Datei vorhanden ist, sonst 404 ausgeben
+// if page doesn't exist, send 404
 if( !file_exists($page) ) {
 	response404();
 }
 
-// Prüfen, ob entlang des Pfades eine abfangende index.inc.php liegt sonst $page verwenden
+// check if there is a index.inc.php along the path, otherwise use $page
 $path_partial = "";
 for($i = 0; $i<count($path)-1; $i++) {
 	$path_partial .= $path[$i] . "/";
@@ -112,64 +99,3 @@ if(empty($include)) {
 
 </body>
 </html>
-
-<?php
-
-} catch (Exception $e) {
-	
-	if(DEBUG_MODE) {
-		
-		error_log($e);
-		echo nl2br($e);
-	
-	} else {
-			
-		echo "<p>Sorry, there has been an error ...</p>";
-		
-	}
-	
-	if(MAIL_ERRORS) {
-		
-		error_log($e);
-		
-		// fileformat: "[timestamp] [n errors since last email]"
-		$flag_file_path = ABSPATH . "mail_error.flag";
-		
-		if(!file_exists($flag_file_path)) {
-			file_put_contents($flag_file_path, "0 0");
-		} else {
-			list($time, $num) = explode(" ", file_get_contents($flag_file_path));
-			
-			// if last error mail was send less then one hour ago just increase the number flag
-			// ... otherwise reset the time and send the error mail
-			if(time() - $time < 3600) {
-				file_put_contents($flag_file_path, $time . " " . ($num + 1));
-			} else {
-				file_put_contents($flag_file_path, time() . " 0");
-				
-				$emailtext = $num . " errors occured since the last error mail\r\n\r\n";
-				$emailtext .= "ERROR: " . $e . "\r\n\r\n";
-				$emailtext .= "POST-DATA:\r\n";
-				foreach ($_POST as $key => $value){
-					$emailtext .= $key . " = " . $value ."\r\n";
-				}
-				$emailtext .= "\r\n" .
-					'REMOTE_ADDR: ' . (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : "") . "\r\n" .
-					'REMOTE_HOST: ' . (isset($_SERVER['REMOTE_HOST']) ? $_SERVER['REMOTE_HOST'] : "") . "\r\n" .
-					'HTTP_USER_AGENT: ' . (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "") . "\r\n" .
-					'HTTP_REFERER: ' . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "") . "\r\n" .
-					'REQUEST_METHOD: ' . (isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : "") . "\r\n" .
-					'REQUEST_URI: ' . (isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : "") . "\r\n" .
-					'PATH_INFO: ' . (isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : "") . "\r\n" .
-					'QUERY_STRING: ' . (isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : "");
-			
-				mail(
-					"j@sights.de",
-					"Fehler",
-					$emailtext,
-					"From: noreply@siteframe.de"
-				);		
-			}
-		}	
-	}
-}
